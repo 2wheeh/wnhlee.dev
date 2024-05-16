b1bf03f7f1a02534b87d8670413b39d346cacca4
wnhlee <2wheeh@gmail.com>
2024-05-17 01:30:37 +0900

    update html lang code

--- src/config.ts
+++ src/config.ts
@@ -13,8 +13,8 @@ export const SITE: Site = {
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "ko", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default => 시간 표시 용
 } as const;
 
 export const LOGO_IMAGE = {
