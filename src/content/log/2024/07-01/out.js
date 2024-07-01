8810a1cbebd16eb3062dab04e1af00f9d207e47f
wnhlee <2wheeh@gmail.com>
2024-07-01 19:15:33 +0900

    docs(log): 2024/07-01

---
+++ src/content/log/2024/07-01/out.js
@@ -0,0 +1,14 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
+
+const profile = _jsxs("div", {
+  children: [
+    _jsx("img", {
+      src: "avatar.png",
+      className: "profile",
+    }),
+    _jsx("h3", {
+      children: [user.firstName, user.lastName].join(" "),
+    }),
+  ],
+});
